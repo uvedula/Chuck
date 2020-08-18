345e241a5f25d2cb2d9da1fa41c2272123ec678f
Vedula <uvedula@38f9d39cb735.ant.amazon.com>
2020-08-18 18:33:08 -0400

    first commit

---
+++ Chuck/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Chuck.Models;
+using System.Net.Http;
+
+namespace Chuck.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // GET: Joke
+        public async Task<IActionResult> Index()
+        {
+            string apiUrl = "https://api.chucknorris.io/jokes/random";
+
+            using (System.Net.Http.HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(apiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    Joke JsonObjectJoke = Newtonsoft.Json.JsonConvert.DeserializeObject<Joke>(data);
+                    return View(JsonObjectJoke);
+                }
+                else
+                {
+                    return View("Site is not up");
+                }
+
+            }
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Chuck/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Chuck.Models
+{
+    public class Joke
+    { 
+            public string[] categories { get; set; }
+            public string created_at { get; set; }
+            public string icon_url { get; set; }
+            public string id { get; set; }
+            public string updated_at { get; set; }
+            public string url { get; set; }
+            public string value { get; set; }
+    }
+}
